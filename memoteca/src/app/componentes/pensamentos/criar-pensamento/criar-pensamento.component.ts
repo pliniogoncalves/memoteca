7303534f0a01a50d83a071456b6c0bfedf173f6b
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-31 23:37:49 -0300

    iniciando o crud, criar e listar

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,23 +11,27 @@ import { Pensamento } from '../pensamento';
 export class CriarPensamentoComponent implements OnInit {
 
   pensamento: Pensamento = {
-    id:1,
     conteudo: '',
     autoria:'',
     modelo: 'modelo1'
   }
 
-  constructor() { }
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
   }
 
   CriarPensamento(){
-    alert("novo pensamento criado");
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar(){
-    alert("Cancelado");
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
