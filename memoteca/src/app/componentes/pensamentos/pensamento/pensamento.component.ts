c06d61602941abcba4b66b08ca7d13eaf7692d01
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-31 21:46:13 -0300

    criando a lista de pensamentos

--- memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pensamento.component.css']
 })
 export class PensamentoComponent implements OnInit {
+
+  @Input() pensamento = {
+    conteudo: 'I love front',
+    autoria: 'Plinio',
+    modelo: 'modelo3'
+  }
 
   constructor() { }
 
