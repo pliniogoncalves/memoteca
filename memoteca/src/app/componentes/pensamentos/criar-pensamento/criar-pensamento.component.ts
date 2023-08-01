fa8ba6c5c97f7c2ddf1286e8c486ed95c2323ae2
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-31 21:22:08 -0300

    criando rotas entre as paginas

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -24,7 +25,7 @@ export class CriarPensamentoComponent implements OnInit {
   }
 
   cancelar(){
-    alert("ação cancelada");
+    alert("Cancelado");
   }
 
 }
